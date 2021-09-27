18e5a09d3c037608c0508b851909f44acc1df20f
Jarob Heffner <jarobheffner@gmail.com>
2021-09-27 15:19:13 -0700

    added quit button

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import tkinter as tk
 
+def end():
+    quit()
+
+
 window = tk.Tk()
 
-greeting = tk.Label(text="Hello, Tkinter")
+window.rowconfigure(0, minsize=50, weight=1)
+window.columnconfigure([0, 1, 2], minsize=50, weight=1)
 
-greeting.pack()
+btn_decrease = tk.Button(master=window, text="quit", command=end)
+btn_decrease.grid(row=0, column=0, sticky="nsew")
 
 window.mainloop()

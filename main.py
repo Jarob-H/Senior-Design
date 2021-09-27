c52bdb45ee9b63dddf75cb372539c8b2e0c37119
Jarob Heffner <jarobheffner@gmail.com>
2021-09-27 16:05:31 -0700

    edit button

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ width, height = window.winfo_screenwidth(), window.winfo_screenheight()
 window.geometry('%dx%d+0+0' % (width,height))
 
 
-quit_butt = tk.Button(master=window, text="quit", command=end,height= 7,width=14)
+quit_butt = tk.Button(master=window, text="quit", command=end,height= 7,width=14, bg="red",
+    fg="white",padx=100, pady=100)
 
 quit_butt.grid(column=1, row=1)
 

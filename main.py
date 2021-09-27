e01f33369cac44e8546784e05c134bb209ebd8b3
Jarob Heffner <jarobheffner@gmail.com>
2021-09-27 15:23:52 -0700

    added autofit

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import tkinter as tk
+from tkinter import *
 
 def end():
     quit()
 
 
+
 window = tk.Tk()
+width, height = window.winfo_screenwidth(), window.winfo_screenheight()
+
+window.geometry('%dx%d+0+0' % (width,height))
 
 window.rowconfigure(0, minsize=50, weight=1)
 window.columnconfigure([0, 1, 2], minsize=50, weight=1)
 
-btn_decrease = tk.Button(master=window, text="quit", command=end)
-btn_decrease.grid(row=0, column=0, sticky="nsew")
+quit_butt= tk.Button(master=window, text="quit", command=end)
+quit_butt.grid(row=0, column=0, sticky="nsew")
 
 window.mainloop()

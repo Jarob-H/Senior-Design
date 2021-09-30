f6033013b8eda0101b5b97c678bd06921c66c0a9
Jarob Heffner <jarobheffner@gmail.com>
2021-09-29 22:04:36 -0700

    sent example to corey

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ width, height = window.winfo_screenwidth(), window.winfo_screenheight()
 
 window.geometry('%dx%d+0+0' % (width,height))
 
-print_butt=quit_butt = tk.Button(master=window, text="print", command=print_fun,height= 4,width=8, bg="blue",
+print_button=quit_butt = tk.Button(master=window, text="print", command=print_fun,height= 4,width=8, bg="blue",
     fg="white",padx=100, pady=100)
 
 
-quit_butt = tk.Button(master=window, text="quit", command=end,height= 4,width=8, bg="red",
+quit_button = tk.Button(master=window, text="quit", command=end,height= 4,width=8, bg="red",
     fg="white",padx=100, pady=100)
 
-print_butt.grid(column=0, row=0)
+print_button.grid(column=0, row=0)
 quit_butt.grid(column=1, row=0)
 
 
 window.mainloop()
 
 print("Hello Jarob")
+print("hello corey")

852a67bcf794c3fc779dd856cec41297e47aa115
Jarob Heffner <jarobheffner@gmail.com>
2021-10-02 05:59:41 -0700

    added speed buttons

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import tkinter as tk #lib for ui
+#three default speeds,off/cutom
 
 def end(): #funtc called by quit button
     quit()
 
-def print_fun():#funtc called by print button
-    print("hello")
+def low():#funtc called by print button
+    print("Speed: Low")
+def med():#funtc called by print button
+    print("Speed: Med")
+def high():#funtc called by print button
+    print("Speed: High")
 
 window = tk.Tk() #creating window ui instance
 width, height = window.winfo_screenwidth(), window.winfo_screenheight() #setting ui screen to fill screen
 
 window.geometry('%dx%d+0+0' % (width,height))#setting ui size
 
-print_button= tk.Button(master=window, text="Hit to print", command=print_fun,height= 4,width=8, bg="blue",
+low_button= tk.Button(master=window, text="LOW", command=low,height= 4,width=8, bg="blue",
+    fg="white",padx=100, pady=100)
+med_button= tk.Button(master=window, text="MED", command=med,height= 4,width=8, bg="blue",
+    fg="white",padx=100, pady=100)
+high_button= tk.Button(master=window, text="High", command=high,height= 4,width=8, bg="blue",
     fg="white",padx=100, pady=100)
 
 
-quit_button = tk.Button(master=window, text="quit", command=end,height= 4,width=8, bg="red",
+
+off_button = tk.Button(master=window, text="OFF", command=end,height= 4,width=8, bg="red",
     fg="white",padx=100, pady=100)
 
-print_button.grid(column=0, row=0)
-quit_button.grid(column=2, row=0)
+low_button.grid(column=0, row=0)
+med_button.grid(column=1, row=0)
+high_button.grid(column=2, row=0)
+off_button.grid(column=1, row=1)
 
 
 window.mainloop() #runs ui

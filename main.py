872a90061372905947ffdd741d3ddb15a49c8b7c
Jarob <jarobheffner@gmail.com>
2021-12-02 00:31:15 -0700

    started working on UI custom speed/duration button

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
-import tkinter as tk #lib for ui
-#three default speeds,off/cutom
+import tkinter as tk  # lib for ui
+
+
+# three default speeds,off/cutom
 
 
 class UI:
-
     def __init__(self):
-        self.time=0
-        self.speed=0
-        self.windowwidth=29
-        self.windowHeight=10
+        self.time = 0
+        self.speed = 0
+        self.buttonWidth = 29
+        self.buttonHeight = 10
+        self.windowHeight=0
+        self.windowWidth=0
         self.window()
 
     def window(self):
-        window = tk.Tk() #creating window ui instance
-        width, height = window.winfo_screenwidth(), window.winfo_screenheight() #setting ui screen to fill screen
+        window = tk.Tk()  # creating window ui instance
+        self.windowWidth, self.windowHeight = window.winfo_screenwidth(), window.winfo_screenheight()  # setting ui screen to fill screen
 
-        window.geometry('%dx%d+0+0' % (width,height))#setting ui size
+        window.geometry('%dx%d+0+0' % (self.windowWidth, self.windowHeight))  # setting ui size
 
-        low_button= tk.Button(master=window, text="LOW", command=self.low,height= self.windowHeight , width=self.windowwidth, bg="blue",
-            fg="white")
-        med_button= tk.Button(master=window, text="MED", command=self.med,height= self.windowHeight ,width=self.windowwidth, bg="blue",
-            fg="white")
-        high_button= tk.Button(master=window, text="High", command=self.high,height= self.windowHeight ,width=self.windowwidth, bg="blue",
-            fg="white")
+        low_button = tk.Button(master=window, text="LOW", command=self.low, height=self.buttonHeight,
+                               width=self.buttonWidth, bg="blue",
+                               fg="white")
+        med_button = tk.Button(master=window, text="MED", command=self.med, height=self.buttonHeight,
+                               width=self.buttonWidth, bg="blue",
+                               fg="white")
+        high_button = tk.Button(master=window, text="High", command=self.high, height=self.buttonHeight,
+                                width=self.buttonWidth, bg="blue",
+                                fg="white")
 
-        off_button = tk.Button(master=window, text="OFF", command=self.end,height= self.windowHeight ,width=self.windowwidth, bg="red",
-            fg="white")
+        custom_button = tk.Button(master=window, text="CUSTOM", command=self.open_popup, height=self.buttonHeight,
+                               width=self.buttonWidth, bg="yellow",
+                               fg="white")
 
-        low_button.grid(column=0, row=0,padx=2, pady=5)
-        med_button.grid(column=1, row=0,padx=2)
-        high_button.grid(column=2, row=0,padx=2)
-        off_button.grid(column=1, row=1)
+        off_button = tk.Button(master=window, text="OFF", command=self.end, height=self.buttonHeight,
+                               width=self.buttonWidth, bg="red",
+                               fg="white")
 
-        window.mainloop() #runs ui
+        low_button.grid(column=0, row=0, padx=2, pady=5)
+        med_button.grid(column=1, row=0, padx=2)
+        high_button.grid(column=2, row=0, padx=2)
+        custom_button.grid(column=1, row=1)
+        off_button.grid(column=2, row=1)
+
+        window.mainloop()  # runs ui
 
     def end(self):  # funtc called by quit button
         quit()
@@ -46,8 +58,20 @@ class UI:
     def high(self):  # funtc called by print button
         print("Speed: High")
 
+    def open_popup(self):
+        popup=tk.Tk()
+        popup.geometry('%dx%d+0+0' % (self.windowWidth,self.windowHeight))  # setting ui size
+        label = tk.Label(popup, text='error')
+        label.pack(side="top", fill="x", pady=10)
+        B1 = tk.Button(popup, text="Okay", command=popup.destroy)
+        B1.pack()
+        popup.mainloop()
+
+
 def main():
     print("Hello World!")
-    ui =UI()
+    ui = UI()
+
+
 if __name__ == "__main__":
     main()
